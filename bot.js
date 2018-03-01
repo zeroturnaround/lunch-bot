a232b6ea9036a8ab229d4e293be205dd089cf79e
ValeriePtah <>
2018-03-01 14:41:59 +0200

    removed FB duplicate

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ const sources = [
     new Facebook('unicorn_face', 'Pegasus', "RestoranPegasus"),
     new Facebook('bike', 'Rataskaevu 16', 'Rataskaevu16'),
     new Facebook('hole', 'Väike Rataskaevu 16', 'VaikeRataskaevu16'),
-    new Facebook('leaves', 'Rahva Toit', 'rahvatoitttu'),
+    // new Facebook('leaves', 'Rahva Toit', 'rahvatoitttu'),
     new Link('leaves', 'Rahva toit', 'Come and eat', 'http://rahvatoit.ee/menuu/rahva-toit-roosikrantsi/')
-  // new Facebook('elephant', 'Restoran Elevant', 'restoran.elevant'),
+    // new Facebook('elephant', 'Restoran Elevant', 'restoran.elevant'),
 ];
 
 const lunchbot = new LunchBot({
